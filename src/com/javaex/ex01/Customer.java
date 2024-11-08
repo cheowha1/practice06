ba6610f347a2be4d0883c0258839d8a859ff7b14
cheowha1 <cheowha1@naver.com>
2024-11-08 17:22:50 +0900

    practice06

---
+++ src/com/javaex/ex01/Customer.java
@@ -0,0 +1,18 @@
+package com.javaex.ex01;
+
+public class Customer extends Person{
+	// 필드
+	private String name;
+	private String hp;
+	
+	// 생성자
+	public Customer() {
+		super();
+		this.name = name;
+		this.hp = hp;
+		System,
+		
+	}
+
+	
+}
