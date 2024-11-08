552605c09bd4878fa18af1dcb17c0e0707a68485
cheowha1 <cheowha1@naver.com>
2024-11-08 18:04:20 +0900

    다시

--- src/com/javaex/ex01/Customer.java
+++ src/com/javaex/ex01/Customer.java
@@ -2,17 +2,27 @@ package com.javaex.ex01;
 
 public class Customer extends Person{
 	// 필드
-	private String name;
-	private String hp;
+	private String personName;
 	
-	// 생성자
-	public Customer() {
+	//생성자
+	public Customer(String personName) {
 		super();
-		this.name = name;
-		this.hp = hp;
-		System,
-		
+		this.personName = personName;
+		System.out.println("Customer(String, int String) 생성자 호출");
 	}
-
+	
+	// Getters/Setters
+	public String getPersonName() {
+		return personName;
+	}
+	
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+	
+	@Override
+	public void showInfo() {
+		System.out.println("이름:" + name + ", 핸드폰:"+ hp);
+	}
 	
 }
--- src/com/javaex/ex01/Person.java
+++ src/com/javaex/ex01/Person.java
@@ -1,7 +1,38 @@
 package com.javaex.ex01;
 
 public class Person {
+	// 필드
+	protected String name;
+	protected String hp;
 	
+	// 생성자
+	public Person() {
+		System.out.println("Person() 생성자 호출");
+	}
 	
+	public Person(String name, String hp) {
+		this.name = name;
+		this.hp = hp;
+		System.out.print("Person(String, int) 생성자 호출");
+		
+	}
+	
+	// Getters/Setters
+	public String getName() {
+		return name;
+	}
+	
+	public String getHp() {
+		return hp;
+	}
+	
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	
+	// 일반 메서드
+	public void showInfo() {
+		System.out.println("이름:" + name + ", 핸드폰:" + hp);
+	}
 	
 }
